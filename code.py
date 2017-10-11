d4009e4a3fc712704fe0d96e2c1db651f81ec4b5
Melvin Mathew <melvin15may@gmail.com>
2017-10-10 19:19:07 -0700

    added documentation

--- code.py
+++ code.py
@@ -7,6 +7,17 @@ RANDOM_URL = "https://www.random.org/{type}/"
 
 def get_random_integers(num, value_min, value_max, col, base=10, return_format="plain", rnd="new"):
     """
+	Returns a list of random numbers
+
+	Arguments
+	num: Number of random numbers
+	value_min: Min value
+	value_max: Max value
+	col: Number of columns
+	base: Base of number returned
+	return_format: Return type of document
+	rnd: Randomization used
+
     return List
     """
 
@@ -29,7 +40,18 @@ def get_random_integers(num, value_min, value_max, col, base=10, return_format="
         return []
 
 
-def create_rgb_bitmap(pixel_red,pixel_green,pixel_blue, file_name="result.bmp", resolution=[128, 128]):
+def create_rgb_bitmap(pixel_red, pixel_green, pixel_blue, file_name="result.bmp", resolution=[128, 128]):
+	"""
+	Saves a RGB bitmap image
+
+	Arguments
+	pixel_red: list of red pixels
+	pixel_green: list of red pixels
+	pixel_blue: list of red pixels
+	file_name: Name of image
+	resolution: resolution of image
+
+	"""
 
     img = Image.new('RGB', tuple(resolution))
     img_pixels = img.load()
@@ -45,14 +67,11 @@ def create_rgb_bitmap(pixel_red,pixel_green,pixel_blue, file_name="result.bmp", 
 
 
 def main():
-
-    # Main
     total_pixels = 16384 # 128 * 128
     pixel_red = get_random_integers(10000,0,255,1) + get_random_integers(total_pixels - 10000,0,255,1)
     pixel_green = get_random_integers(10000,0,255,1) + get_random_integers(total_pixels - 10000,0,255,1)
     pixel_blue = get_random_integers(10000,0,255,1) + get_random_integers(total_pixels - 10000,0,255,1)
     return create_rgb_bitmap(pixel_red,pixel_green,pixel_blue)
 
+
 main()
-if __name__ == 'main()':
-	main()
